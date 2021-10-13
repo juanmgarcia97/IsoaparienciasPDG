14530909a9d4a4b84681642d60fa05a347179749
Juan Martin Garcia <martin.garcia@iterrabyte.com>
2021-10-13 15:09:47 -0500

    Avances del proyecto Ya se puede visualizar la imagen correctamente. El servicio de google vision ya esta conectado con el front.

--- FrontTest.py
+++ FrontTest.py
@@ -2,38 +2,42 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
 import os
+import VisionAPIDemo as api
 
 root = Tk()
 root.title('Isoapariencias de medicamentos inyectables')
 root.geometry("500x400")
-
-
-def rcpath(rel_path):
-    return os.path.join(os.getcwd(), rel_path)
-
-
-root.iconbitmap(rcpath('Images/icons8-capsule-64.ico'))
+root.iconbitmap(r'Images/icons8-capsule-64.ico')
 
 frame = LabelFrame(root, padx=2, pady=2)
 frame.pack(padx=2, pady=2)
-frame2 = LabelFrame(root, padx=2, pady=2)
-frame2.pack(padx=2, pady=2)
+imageIso = LabelFrame(root, padx=2, pady=2)
+imageIso.pack(padx=2, pady=2)
+resultIso = LabelFrame(root, padx=2, pady=2)
+resultIso.pack()
 
 
 def openFile():
-    global image_label
-    filename = filedialog.askopenfilename(
-        title="Seleccionar imagen", filetypes=(("PNG Files", "*.png"), ("All files", "*.*")))
-    label = Label(frame2, text=filename).pack()
-    uploaded_image = ImageTk.PhotoImage(Image.open(filename))
-    image_label = Label(frame2, image=upload_image).pack()
+    global image_label, uploaded_image, filename
+    filename = filedialog.askopenfilename(title="Seleccionar imagen")
+    label = Label(imageIso, text=filename.rsplit("/")[-1]).pack()
+    uploaded_image = ImageTk.PhotoImage(
+        Image.open(filename).resize([200, 300]))
+    image_label = Label(imageIso, image=uploaded_image).pack()
+    image_label.image = uploaded_image
+
+
+def findIso():
+    data = api.processImage(filename)[0]
+    labelData = Label(resultIso, text=data).pack()
 
 
 upload_image = Button(frame, text="Cargar Imagen",
                       command=openFile).grid(row=0, column=0)
 chose_med = Button(frame, text="Elegir medicamento").grid(row=2, column=0)
 delete_image = Button(frame, text="Eliminar imagen").grid(row=1, column=1)
-find_iso = Button(frame, text="Encontrar isoapariencias").grid(row=1, column=2)
+find_iso = Button(frame, text="Encontrar isoapariencias",
+                  command=findIso).grid(row=1, column=2)
 gen_report = Button(frame, text="Generar reporte").grid(row=1, column=3)
 
 root.mainloop()
--- VisionAPIDemo.py
+++ VisionAPIDemo.py
@@ -7,34 +7,35 @@ import pandas as pd
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceToken.json'
 client = vision.ImageAnnotatorClient()
 
-FOLDER_PATH = r'C:\Users\juanm\Documents\ICESI\9no\Proyecto de Grado I\Proyecto\Codigo\GoogleVisionDemo\Images'
-IMAGE_FILE = 'testImage.jpeg'
-FILE_PATH = os.path.join(FOLDER_PATH, IMAGE_FILE)
+# FOLDER_PATH = r'C:\Users\juanm\Documents\ICESI\9no\Proyecto de Grado I\Proyecto\Codigo\GoogleVisionDemo\Images'
+# IMAGE_FILE = 'testImage.jpeg'
+# FILE_PATH = os.path.join(FOLDER_PATH, IMAGE_FILE)
 
-with io.open(FILE_PATH, 'rb') as image_file:
-    content = image_file.read()
+def processImage(file_path):
+    with io.open(file_path, 'rb') as image_file:
+        content = image_file.read()
 
-image = vision.Image(content=content)
-response = client.document_text_detection(image=image)
+    image = vision.Image(content=content)
+    response = client.document_text_detection(image=image)
 
-docText = response.full_text_annotation.text
-print(docText)
+    docText = response.full_text_annotation.text
+    return docText, response
 
+def processImageAllData(file_path):
+    pages = processImage(file_path)[1].pages
+    for page in pages:
+        for block in page.blocks:
+            print('block confidence:', block.confidence)
 
-pages = response.full_text_annotation.pages
-for page in pages:
-    for block in page.blocks:
-        print('block confidence:', block.confidence)
+            for paragraph in block.paragraphs:
+                print('paragraph confidence:', paragraph.confidence)
 
-        for paragraph in block.paragraphs:
-            print('paragraph confidence:', paragraph.confidence)
+                for word in paragraph.words:
+                    word_text = ''.join([symbol.text for symbol in word.symbols])
 
-            for word in paragraph.words:
-                word_text = ''.join([symbol.text for symbol in word.symbols])
+                    print('Word text: {0} (confidence: {1}'.format(
+                        word_text, word.confidence))
 
-                print('Word text: {0} (confidence: {1}'.format(
-                    word_text, word.confidence))
-
-                for symbol in word.symbols:
-                    print('\tSymbol: {0} (confidence: {1}'.format(
-                        symbol.text, symbol.confidence))
+                    for symbol in word.symbols:
+                        print('\tSymbol: {0} (confidence: {1}'.format(
+                            symbol.text, symbol.confidence))
