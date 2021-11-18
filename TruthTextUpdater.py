6463b5697ed3644a4a11e0ef07d6c49feba57a79
juanmgarcia97 <juanmar-p@hotmail.com>
2021-11-17 23:53:28 -0500

    TruthText updated
    Review special characters

--- TruthTextUpdater.py
+++ TruthTextUpdater.py
@@ -15,26 +15,26 @@ count = 0
 images_path = r'Images'
 images_results = []
 
-for image in list_files:
-    aux_name = '_MG_' + str(count) + '.jpg'
-    file_path = os.path.join(os.path.dirname(__file__), images_path, aux_name)
-    with io.open(file_path, 'rb') as image_file:
-        content = image_file.read()
+# for image in list_files:
+#     aux_name = '_MG_' + str(count) + '.jpg'
+#     file_path = os.path.join(os.path.dirname(__file__), images_path, aux_name)
+#     with io.open(file_path, 'rb') as image_file:
+#         content = image_file.read()
 
-    image = vision.Image(content=content)
-    response = client.document_text_detection(image=image)
-    docText = response.full_text_annotation.text
+#     image = vision.Image(content=content)
+#     response = client.document_text_detection(image=image)
+#     docText = response.full_text_annotation.text
 
-    texts = docText.split('\n')
-    finalText = ''
+#     texts = docText.split('\n')
+#     finalText = ''
 
-    for text in texts:
-        finalText = finalText + text + ';'
+#     for text in texts:
+#         finalText = finalText + text + ';'
 
-    print(finalText + SPLITTER + aux_name)
+#     print(finalText + SPLITTER + aux_name)
 
-    images_results.append(finalText + SPLITTER + aux_name + '\n')
-    count += 1
+#     images_results.append(finalText + SPLITTER + aux_name + '\n')
+#     count += 1
 
-truth_text.writelines(images_results)
+truth_text.writelines("了無")
 truth_text.close()
