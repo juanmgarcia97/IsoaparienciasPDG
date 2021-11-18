4593461c1e7e9ecf8e94aa3794c44304b3667492
juanmgarcia97 <juanmar-p@hotmail.com>
2021-11-17 23:24:12 -0500

    TruthText automated

--- ChangeFileNames.py
+++ ChangeFileNames.py
@@ -1,5 +1,16 @@
 import os
 
-list_files = os.listdir()
+list_files = os.listdir(r'Images')
 
-os.rename()
+# Este hpt método cambia el nombre de archivos de manera masiva
+
+count = 0
+for image in list_files:
+    images_path = r'Images'
+    aux_name = '_MG_' + str(count) + '.jpg'
+    old_name = os.path.join(os.path.dirname(__file__), images_path, image)
+    new_name = os.path.join(os.path.dirname(__file__), images_path, aux_name)
+    os.rename(old_name, new_name)
+    count += 1
+
+
